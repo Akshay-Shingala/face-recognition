30b432c1f1b172b3e48364c06d4c7fa0f05f33f3
Akshay-Shingala <131964978+Akshay-Shingala@users.noreply.github.com>
2024-03-21 10:40:44 +0530

    fix : fix base image bug

--- run.py
+++ run.py
@@ -8,33 +8,23 @@ app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
 def upload_images():
+    base = request.files.get('base',request.form.get('base',False))
     file1 = request.files.getlist('file1')
     url_list = request.form.getlist("file1")
     file1.extend(url_list)
-    image_list = [f for f in file1 if face_count(f ,is_url=type(f) == str) == 1]
+    compare_list = [f for f in file1 if face_count(f ,is_url=type(f) == str) == 1]
     
-    print(image_list)
-    # # Url  place of image 
-    # url='https://cdn.zeebiz.com/sites/default/files/2023/10/05/263573-untitled-design-2023-10-05t091024122.jpg'
-    # url="https://yudiz-python-s3.s3.ap-south-1.amazonaws.com/python-projects/InstaAI/deforum_motion_effect/amitabh_bachchan.jpeg"
-    # print(face_count(url, is_url=True))
-    
-    if len(image_list) < 2:
+    if face_count(base,is_url = type(base) == str) != 1:
+        return "plese enter proper base image"
+    if len(compare_list) < 1:
         return 'Please upload four image files with exactly one face in each image.', 400
-    
-    base = image_list[0]
-    compare_list = image_list[1:]
     results = face_compare(base,compare_list)
-    print(results)
     return str(results)
 
 def face_count(image, is_url=False):
     try:
-        print(image)
         image = face_recognition.load_image_file(image if not is_url else urlopen(image))
         face_locations = face_recognition.face_locations(image)
-        print(face_locations)
-
         return len(face_locations)
     except Exception as e:
         print("Error:",e)
@@ -46,7 +36,6 @@ def face_compare(base,images):
     
     base_encoding = face_recognition.face_encodings(base_img)[0]
     compare_img_encoding_list = [ face_recognition.face_encodings(img)[0] for img in compare_img_list ]
-    print(compare_img_encoding_list)
     results = face_recognition.compare_faces(compare_img_encoding_list, base_encoding)
     return results
 
